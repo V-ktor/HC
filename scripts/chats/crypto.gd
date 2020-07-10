b4f75340ebbbf1240f25a3d0b5f655881527129f
Viktor Hahn <viktor.hahn@web.de>
2020-07-10 20:58:23 +0200

    The Crypto chat no longer grants stat points.

--- scripts/chats/crypto.gd
+++ scripts/chats/crypto.gd
@@ -3,9 +3,9 @@ extends Node
 func chat01():
 	Events.delayed_msg("crypto",tr("CRYPTO_0001"),1.0)
 	Events.delayed_choice("crypto",[
-		{"text":"REPLY_0030_1","required":{"charisma":3},"personality":{"charisma":1}},
-		{"text":"REPLY_0030_2","required":{"focus":3},"personality":{"focus":1}},
-		{"text":"REPLY_0030_3","required":{"cunning":3},"personality":{"cunning":1}}
+		{"text":"REPLY_0030_1","required":{"charisma":3}},
+		{"text":"REPLY_0030_2","required":{"focus":3}},
+		{"text":"REPLY_0030_3","required":{"cunning":3}}
 	],2.0)
 	Menu.add_log_msg("LOG_CRYPTO","LOG_MET_CRYPTO")
 	Music.play("Of_Far_Different_Nature-Escape-10-Control.ogg",-4)
@@ -16,8 +16,8 @@ func reply_0030_1():
 	Events.delayed_msg("crypto",tr("CRYPTO_0002"),1.0)
 	Events.delayed_msg("crypto",tr("CRYPTO_0003"),2.0)
 	Events.delayed_choice("crypto",[
-		{"text":"REPLY_0031_1","personality":{"curiosity":1}},
-		{"text":"REPLY_0031_2","personality":{"cunning":1}}
+		{"text":"REPLY_0031_1","required":{"curiosity":1}},
+		{"text":"REPLY_0031_2","required":{"cunning":1}}
 	],1.0)
 
 func reply_0031_1():
@@ -37,8 +37,8 @@ func reply_0030_2():
 	Events.delayed_player_msg("crypto",tr("REPLY_0030_2-2"),1.0)
 	Events.delayed_msg("crypto",tr("CRYPTO_0008"),2.0)
 	Events.delayed_choice("crypto",[
-		{"text":"REPLY_0032_1","personality":{"curiosity":1}},
-		{"text":"REPLY_0032_2","personality":{"charisma":1}}
+		{"text":"REPLY_0032_1","required":{"curiosity":1}},
+		{"text":"REPLY_0032_2","required":{"charisma":1}}
 	],1.0)
 
 func reply_0032_1():
@@ -63,8 +63,8 @@ func reply_0030_3():
 	Events.delayed_msg("crypto",tr("CRYPTO_0012"),1.0)
 	Events.delayed_msg("crypto",tr("CRYPTO_0013"),2.0)
 	Events.delayed_choice("crypto",[
-		{"text":"REPLY_0033_1","personality":{"charisma":1}},
-		{"text":"REPLY_0033_2","personality":{"cunning":1}}
+		{"text":"REPLY_0033_1","required":{"charisma":1}},
+		{"text":"REPLY_0033_2","required":{"cunning":1}}
 	],1.0)
 
 func reply_0033_1():
@@ -82,9 +82,9 @@ func reply_0033_2():
 
 func next_try():
 	Events.delayed_choice("crypto",[
-		{"text":"REPLY_0034_1","required":{"focus":3},"personality":{"focus":1}},
-		{"text":"REPLY_0034_2","required":{"cunning":3},"personality":{"cunning":1}},
-		{"text":"REPLY_0034_3","required":{"fear":3},"personality":{"fear":1}}
+		{"text":"REPLY_0034_1","required":{"focus":3}},
+		{"text":"REPLY_0034_2","required":{"cunning":3}},
+		{"text":"REPLY_0034_3","required":{"fear":3}}
 	],1.0)
 
 func reply_0034_1():
@@ -92,8 +92,8 @@ func reply_0034_1():
 	Events.delayed_msg("crypto",tr("CRYPTO_0020"),1.0)
 	Events.delayed_msg("crypto",tr("CRYPTO_0021"),1.0)
 	Events.delayed_choice("crypto",[
-		{"text":"REPLY_0035_1","personality":{"charisma":1}},
-		{"text":"REPLY_0035_2","personality":{"curiosity":1}}
+		{"text":"REPLY_0035_1","required":{"charisma":1}},
+		{"text":"REPLY_0035_2","required":{"curiosity":1}}
 	],1.0)
 
 func reply_0035_1():
@@ -113,8 +113,8 @@ func reply_0034_2():
 	Events.delayed_player_msg("crypto",tr("REPLY_0034_2-2"),1.0)
 	Events.delayed_msg("crypto",tr("CRYPTO_0025"),2.0)
 	Events.delayed_choice("crypto",[
-		{"text":"REPLY_0036_1","personality":{"focus":1}},
-		{"text":"REPLY_0036_2","personality":{"curiosity":1}}
+		{"text":"REPLY_0036_1","required":{"focus":1}},
+		{"text":"REPLY_0036_2","required":{"curiosity":1}}
 	],1.0)
 
 func reply_0036_1():
@@ -136,8 +136,8 @@ func reply_0034_3():
 	Events.delayed_msg("crypto",tr("CRYPTO_0030"),1.0)
 	Events.delayed_msg("crypto",tr("CRYPTO_0031"),2.0)
 	Events.delayed_choice("crypto",[
-		{"text":"REPLY_0037_1","personality":{"curiosity":1}},
-		{"text":"REPLY_0037_2","personality":{"charisma":1}}
+		{"text":"REPLY_0037_1","required":{"curiosity":1}},
+		{"text":"REPLY_0037_2","required":{"charisma":1}}
 	],1.0)
 
 func reply_0037_1():
